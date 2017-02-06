4151daee83c7ff5c10eeb4a8ec4a94bc119a1554
Nicolas Wu <wuxin53@crc.com.hk>
2017-02-06 10:17:25 +0800

    Add a few of classes for trial

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+/**
+ * Created by wuxin53 on 2017/2/4.
+ */
+public class App {
+    public static void main(String args[]){
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/TestApp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by wuxin53 on 2017/2/6.
+ */
+public class TestApp {
+    private App app;
+
+    private void testMain(){
+        app = new App();
+    }
+}
